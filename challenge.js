a2b5feb199704c745b510997b9261c6c0d9dbc16
Collins Tatang <makungongcollins@gmail.com>
2021-09-02 08:34:37 +0100

    morning session challenge

---
+++ challenge.js
@@ -0,0 +1,37 @@
+// Start with your code from LinkedList challenge.
+
+const newArr = [];
+class Queue {
+    constructor(val) {
+        this.value = val;
+    }
+    add(number) {
+        return newArr.push(number);
+    }
+    remove() {
+        if (newArr.length === 0) { return -1 };
+        return newArr.shift();
+    }
+}
+const queue = new Queue()
+
+queue.add(3)
+queue.add(5)
+console.log(queue.remove())
+    // => 3
+
+queue.add(2)
+queue.add(7)
+console.log(queue.remove())
+    // => 5
+
+console.log(queue.remove())
+    // => 2
+
+console.log(queue.remove())
+    // => 7
+
+console.log(queue.remove())
+    // => -1
+
+module.exports = Queue
